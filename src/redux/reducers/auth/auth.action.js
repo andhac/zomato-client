c6fc9a882a48cf421d706e81d680e7427730cc66
andhac <andhac84@yahoo.com>
2022-01-31 20:03:31 +0530

    ..

--- src/redux/reducers/auth/auth.action.js
+++ src/redux/reducers/auth/auth.action.js
@@ -52,7 +52,8 @@ export const signOut = () => async (dispatch) => {
   try {
     localStorage.removeItem("zomatoUser");
     clearUser();
-    window.location.href = "http://localhost:3000/delivery";
+    window.location.href =
+     "https://61f7f07a8ed433d02eda7dda--suspicious-jones-03313a.netlify.app/delivery";
 
     return dispatch({ type: SIGN_OUT, payload: {} });
   } catch (error) {
